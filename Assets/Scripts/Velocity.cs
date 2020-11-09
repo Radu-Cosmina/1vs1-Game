2761103e5d3a6e62fa75ed4189662b88350cc889
Cosmina Radu <radu.cosmina9d@yahoo.com>
2020-11-09 13:50:32 +0200

    my first commit

---
+++ Assets/Scripts/Velocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+    // Start is called before the first frame update
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Force;
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Force;
+
+    }
+}
